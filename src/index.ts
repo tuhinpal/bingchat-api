bc84453fb28b42d729d665d288f867a9b6939c26
tuhinpal <me@thetuhin.com>
2023-02-24 22:40:41 +0530

    Added package for client

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.post("/createConversation", createConversation);
+app.post("/create-conversation", createConversation);
 app.post("/generate", generate);
 app.post("/set-cookie", setCookie);
 
